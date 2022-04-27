15eeaefdb9c0cfba8c9ccad11f6fef54fe11bb1a
Ezequiel Pereira <ezequielpereira92@gmail.com>
2022-04-27 17:52:43 -0300

    add product form image preview

--- js/addProduct.js
+++ js/addProduct.js
@@ -1,10 +1,50 @@
+import { handleFiles } from "./handleFile.js";
 import { validateInputs } from "./validate.js";
 
-const addProductInputs = document.querySelectorAll('[product-form-input]');
+const addProductInputs = document.querySelectorAll("[product-form-input]");
 const productSubmit = document.getElementById("product-submit");
 const contactInputs = document.querySelectorAll("[contact-form-input]");
 const submitButton = document.getElementById("contact-submit");
+const dropZone = document.getElementById("drop-zone");
+const addImgInput = document.getElementById("add-img-input");
+const addImgButton = document.getElementById("add-img-button");
 
+dropZone.addEventListener("dragover", dragOverHandler, false);
+dropZone.addEventListener("drop", dropHandler, false);
+
+function dropHandler(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  const dt = e.dataTransfer;
+  const files = dt.files;
+
+  handleFiles(files);
+}
+
+function dragOverHandler(e) {
+  e.preventDefault();
+}
+
+addImgButton.addEventListener(
+  "click",
+  function (e) {
+    if (addImgInput) {
+      addImgInput.click();
+    }
+    e.preventDefault();
+  },
+  false
+);
+
+function handleInput(e) {
+  if (e.target.files) {
+    handleFiles(e.target.files);
+  } else {
+    console.log("no files :(");
+  }
+}
+
+addImgInput.addEventListener("change", handleInput, false);
 validateInputs(addProductInputs, productSubmit);
-
-validateInputs(contactInputs, submitButton)
+validateInputs(contactInputs, submitButton);
---
+++ js/handleFile.js
@@ -0,0 +1,21 @@
+export function handleFiles(files) {
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+
+    if (!file.type.startsWith("image/")) {
+      continue;
+    }
+
+    const preview = document.getElementById("preview");
+    preview.style.display = "block";
+    const img = document.getElementById("preview-img");
+
+    const reader = new FileReader();
+    reader.onload = (function (aImg) {
+      return function (e) {
+        aImg.src = e.target.result;
+      };
+    })(img);
+    reader.readAsDataURL(file);
+  }
+}
