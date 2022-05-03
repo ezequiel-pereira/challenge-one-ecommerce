43595be4927cd0eee9d59024a836c79a59152f87
Ezequiel Pereira <ezequielpereira92@gmail.com>
2022-05-03 19:00:47 -0300

    render products from json-server

---
+++ js/controller/product.js
@@ -0,0 +1,26 @@
+import { getProducts } from "../service/product.js";
+
+const products = document.getElementById("products-list");
+
+const renderProduct = (product) => {
+  const productDiv = document.createElement("div");
+  productDiv.classList.add("item");
+  const content = `
+    <img class="item-img" src="${product.img}" alt="${product.description}">
+    <h3 class="item-title">${product.name}</h3>
+    <p class="item-price">${product.price}</p>
+    <button class="item-button">Ver producto</button>`;
+  productDiv.innerHTML = content;
+  return productDiv;
+};
+
+getProducts()
+  .then((data) => {
+    data.forEach((element) => {
+      const newProduct = renderProduct(element);
+      products.appendChild(newProduct);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
--- js/service/product.js
+++ js/service/product.js
@@ -1,27 +1,7 @@
-const http = new XMLHttpRequest();
-
 const getProducts = () => {
-  const promise = new Promise((resolve, reject) => {
-    http.open("GET", "http://localhost:3000/products");
-    http.send();
-    http.onload = () => {
-      const response = JSON.parse(http.response);
-      if (http.status >= 400) {
-        reject(response);
-      } else {
-        resolve(response);
-      }
-    };
+  return fetch("http://localhost:3000/products").then((response) => {
+    return response.json();
   });
-  return promise;
 };
 
-getProducts()
-  .then((data) => {
-    data.forEach((element) => {
-      console.log(element);
-    });
-  })
-  .catch((e) => {
-    console.log("shit");
-  });
+export { getProducts };
